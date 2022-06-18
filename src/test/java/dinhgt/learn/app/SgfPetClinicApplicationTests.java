07063e1a4d4de5057540bf0a243e609ad813b578
GDante <dinhgiang443@gmail.com>
2022-06-18 18:09:52 +0700

    Create Project

---
+++ src/test/java/dinhgt/learn/app/SgfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package dinhgt.learn.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgfPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
